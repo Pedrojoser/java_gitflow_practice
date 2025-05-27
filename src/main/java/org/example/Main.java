6f4e1acc7505e23764a5b22762d3ed3368aedd73
Pedro Romero <pedro.romero@gmail.com>
2025-05-27 17:39:26 -0500

    Agregar comentario en feature/comentario2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-// Comentario: Primera funcionalidad agregada en feature/comentario1
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        // ComentarioPrimera funcionalidad agregada en feature/comentario1:
+        // Comentario: Segunda funcionalidad agregada en feature/comentario2
+
     }
 }
